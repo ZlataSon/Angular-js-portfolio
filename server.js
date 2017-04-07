546824372eb9db13f8c35cd777281b68bf6ac5c5
Evgen <e.salyakin@gmail.com>
2017-04-08 00:18:38 +0300

    Zlata deploy

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('./'));
-
-
-app.use(express.static('app/jspm_packages'));
 app.use(express.static(__dirname + "/app"));
+
+
+app.use('/jspm_packages', express.static(__dirname + '/app/jspm_packages'));
+
 //app.use(express.static(__dirname + "/jspm_packages"));
 
 app.listen(process.env.PORT || 3000);
