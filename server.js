cadef3584dc77c072d2fcdb405c786bb85c8072e
Evgen <e.salyakin@gmail.com>
2017-04-08 00:08:26 +0300

    Zlata deploy

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var app = express();
 
 app.use(express.static('./'));
 
+app.use(express.static(__dirname + "/jspm_packages/github/angular/bower-material@master"));
 app.use(express.static(__dirname + "/app"));
 app.use(express.static(__dirname + "/jspm_packages"));
-
+app.use(express.static(__dirname + "app/jspm_packages/system.js"));
 app.listen(process.env.PORT || 3000);
 console.log('Listening on port 3000...');
