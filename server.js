89dee369b2a2e7b55c4198eb5811f369d3eb0cf4
Evgen <e.salyakin@gmail.com>
2017-04-08 00:02:04 +0300

    Zlata deploy

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 app.use(express.static('./'));
 
 app.use(express.static(__dirname + "/app"));
+app.use(express.static(__dirname + "/jspm_packages"));
 
 app.listen(process.env.PORT || 3000);
 console.log('Listening on port 3000...');
